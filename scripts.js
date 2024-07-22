c7f5ea6e389f408c53a520193439414cb0399f90
snessiri <snessiri@gmail.com>
2024-07-22 14:51:31 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
